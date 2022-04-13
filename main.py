efdf93a0155ad3d3caf3e0c33206c95c74b8b4ee
Kevin Goltermann <kggoltermann@gmail.com>
2022-04-12 18:23:07 -0600

    Cleaning up batting table

--- main.py
+++ main.py
@@ -2,25 +2,18 @@ import requests
 import pandas as pd
 import numpy as np
 
-
+## Setting URL to MLB stats site
 
 URL = "https://www.baseball-reference.com/leagues/majors/2022.shtml"
 page = requests.get(URL)
 
-
-df = pd.read_html(URL, attrs = {'id': 'teams_standard_batting'})
-
-
-df=df[0].drop(df[0].index[-3: ])
+## Parsing site for 'Team Standard Batting' table and dropping averages row
+df_batting = pd.read_html(URL, attrs = {'id': 'teams_standard_batting'})
+df_batting=df_batting[0].drop(df_batting[0].index[-3: ])
 
 
-df['G'] = pd.to_numeric(df['G'])
-df['H'] = pd.to_numeric(df['H'])
-
-df['Add'] = df.G + df.H
 
 
-print(df)
 
 
 
